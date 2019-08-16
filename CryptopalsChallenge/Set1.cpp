86915bad7a7003ea7f8286707faadb15a02e101b
Marty <marty@michetti.com>
2019-08-16 14:42:57 -0600

    Initial commit

---
+++ CryptopalsChallenge/Set1.cpp
@@ -0,0 +1,12 @@
+#include <openssl/ssl.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	char c;
+	cout << "Hello world\n";
+	// Pause before exiting
+	cout << "Press any key to continue...\n";
+	getchar();
+	return 0;
+}
